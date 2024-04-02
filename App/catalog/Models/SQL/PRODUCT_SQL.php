4190bf1529795cf3ed92c6652fe29f277d9130cb
Bruno Trindade <bruno_henriquet@live.com>
2024-04-02 11:09:12 -0300

    fixed: identation product_sql

--- App/catalog/Models/SQL/PRODUCT_SQL.php
+++ App/catalog/Models/SQL/PRODUCT_SQL.php
@@ -7,25 +7,36 @@ class PRODUCT_SQL
 
   public static function SELECT_PRODUCTS_HIGHLIGHT(): string
   {
-    $sql = "SELECT p.id as p_id, p.name as product_name, price, discount, 
-                   img.name as image_name, img.path 
+    $sql = "SELECT p.id as p_id, 
+                   p.name as product_name, 
+                   price, 
+                   discount, 
+                   img.name as image_name, 
+                   img.path 
               FROM product as p
-                INNER JOIN img on p.id = img.product_id  
-            WHERE status=:status AND highlight=:highlight 
-            ORDER BY RAND() 
-            LIMIT :limit";
+        INNER JOIN img on p.id = img.product_id  
+             WHERE status=:status 
+               AND highlight=:highlight 
+          ORDER BY RAND() 
+             LIMIT :limit";
 
     return $sql;
   }
 
   public static function SELECT_ALL_PRODUCTS($offset): string
   {
-    $sql = "SELECT p.id AS p_id, p.name, p.price, p.discount, 
-                   img.name AS img_name, img.path
+    $sql = "SELECT p.id AS p_id, 
+                   p.name, 
+                   p.price, 
+                   p.discount, 
+                   img.name AS img_name, 
+                   img.path
               FROM product AS p 
         INNER JOIN img ON p.id = img.product_id  
              WHERE status = :status
-          GROUP BY img.name, img.product_id, img.path
+          GROUP BY img.name, 
+                   img.product_id, 
+                   img.path
              LIMIT ". ITEMS_PAGE . " OFFSET ". $offset;
 
     return $sql;
@@ -61,27 +72,39 @@ class PRODUCT_SQL
 
   public static function SELECT_PRODUCTS_CATEGORY($offset): string
   {
-    $sql = "SELECT p.id AS p_id, p.name, price, discount, 
-                  MIN(img.name) AS img_name, 
-                  MIN(path) AS path, 
-                  c.name AS category_name
-            FROM product AS p 
-              INNER JOIN img ON p.id = img.product_id
-              INNER JOIN category AS c
-               ON p.category_id = c.id
-            WHERE p.status= :status  AND c.id = :category_id
-            GROUP BY p.id, p.name, p.price, p.discount
-            ORDER BY RAND() LIMIT ". ITEMS_PAGE . " OFFSET " . $offset;
+    $sql = "SELECT p.id AS p_id, 
+                   p.name, 
+                   price, 
+                   discount, 
+                   MIN(img.name) AS img_name, 
+                   MIN(path) AS path, 
+                   c.name AS category_name
+              FROM product AS p 
+        INNER JOIN img ON p.id = img.product_id
+        INNER JOIN category AS c
+                ON p.category_id = c.id
+             WHERE p.status= :status  AND c.id = :category_id
+          GROUP BY p.id, 
+                   p.name, 
+                   p.price, 
+                   p.discount
+          ORDER BY RAND() LIMIT ". ITEMS_PAGE . " OFFSET " . $offset;
 
     return $sql;
   }
 
   public static function SELECT_PRODUCT(): string
   {
-    $sql = "SELECT p.id as id_p, p.name as product_name, p.quantity, p.price, p.category_id, p.description, p.discount,
-              GROUP_CONCAT(img.name) as img_name,
-              GROUP_CONCAT(img.path) as path,
-              category.name as category_name
+    $sql = "SELECT p.id as id_p, 
+                   p.name as product_name, 
+                   p.quantity, 
+                   p.price, 
+                   p.category_id, 
+                   p.description, 
+                   p.discount,
+                   category.name as category_name
+                   GROUP_CONCAT(img.name) as img_name,
+                   GROUP_CONCAT(img.path) as path,
               FROM 
                   product as p
               LEFT JOIN 
@@ -91,7 +114,7 @@ class PRODUCT_SQL
               WHERE 
                   p.id = :id
               GROUP BY 
-              p.id, p.name, p.quantity, p.price, p.category_id, p.description, p.discount, category.name
+                  p.id, p.name, p.quantity, p.price, p.category_id, p.description, p.discount, category.name
                LIMIT :limit";
 
     return $sql;
@@ -99,17 +122,20 @@ class PRODUCT_SQL
 
   public static function SELECT_PRODUCT_RELATED(): string
   {
-    $sql = "SELECT p.id AS id_p, p.name AS product_name, p.price, p.discount,
-              MIN(img.name) AS img_name,
-              MIN(img.path) AS path
-           FROM product AS p 
-            INNER JOIN category AS id_c 
-              ON p.category_id = id_c.id
-            INNER JOIN img AS img 
-              ON img.product_id = p.id                       
-           WHERE p.category_id = :category_id
+    $sql = "SELECT p.id AS id_p, 
+                   p.name AS product_name, 
+                   p.price, 
+                   p.discount,
+                   MIN(img.name) AS img_name,
+                   MIN(img.path) AS path
+              FROM product AS p 
+        INNER JOIN category AS id_c 
+                ON p.category_id = id_c.id
+        INNER JOIN img AS img 
+                ON img.product_id = p.id                       
+             WHERE p.category_id = :category_id
           GROUP BY p.id, p.name, p.price, p.discount
-          LIMIT :limit";
+            LIMIT :limit";
 
     return $sql;
   }
